1f51f29854722ad629785cb2982b27d242cad130
Pedro Nishimura <pedro.fnishimura@gmail.com>
2024-08-07 18:45:26 -0300

    first commit

--- app.py
+++ app.py
@@ -23,5 +23,9 @@ df = pd.DataFrame({
 # RENDERIZE OS VALORES DO DATAFRAME df EM UMA TABELA HTML DENTRO DA PÁGINA /table.html (CRIE UM HTML PARA ISSO)
 @app.route('/table')
 def table():
+    tbHtml = df.to_html(index=False) # TRANSFORMANDO O DF EM HTML PARA SER RENDERIZADO NA PÁGINA TABLE, COLOQUEI INDEX FALSE PARA NÃO APARECER O INDICE NA TABELA
 
-    return render_template('table.html', )
+    return render_template('table.html', table=tbHtml)
+
+if __name__ == '__main__': # VERIFICANDO SE ESTÁ NA MAIN PARA ASSIM ADICIONAR O COMANDO RUN DO FLASK E RENDERIZAR AS TELAS.
+    app.run(debug=True)
